669949d86f20f1a2dbfc549759c468d0b3399fd5
Tran Trong Trinh <m5c0traan@gmail.com>
2023-10-02 11:18:33 +0700

    push new files about array , day 1

---
+++ demo_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//int demo_array(){
+    int main (){
+        int ary[10];
+        int i;
+        int total = 0;
+        int max = 0;
+        int min = 0;
+        for (i = 0; i < 0;i++)
+        {
+
+        }
+    }
+//}
